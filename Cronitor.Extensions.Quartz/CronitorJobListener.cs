655a34d7761a5df365ceb859b67d2649247437e5
Erik Hennerfors <erik.hennerfors@obscured.se>
2023-07-05 18:04:01 +0200

    Update(s)

---
+++ Cronitor.Extensions.Quartz/CronitorJobListener.cs
@@ -0,0 +1,50 @@
+﻿using Cronitor.Extensions.Quartz.Attributes;
+using Quartz;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cronitor.Extensions.Quartz
+{
+    public class CronitorJobListener : IJobListener
+    {
+        public string Name => nameof(CronitorJobListener);
+
+        public Task JobExecutionVetoed(IJobExecutionContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task JobToBeExecuted(IJobExecutionContext context, CancellationToken cancellationToken = default)
+        {
+            if (Cronitor.IsConfigured)
+            {
+                var attribute = GetAttribute(context.JobInstance);
+                if (attribute != null && !string.IsNullOrEmpty(attribute.Key))
+                {
+                    await Cronitor.Telemetries.CompleteAsync(attribute.Key, attribute.Message, attribute.Environment);
+                }
+            }
+        }
+
+        public async Task JobWasExecuted(IJobExecutionContext context, JobExecutionException jobException, CancellationToken cancellationToken = default)
+        {
+            if (Cronitor.IsConfigured)
+            {
+                var attribute = GetAttribute(context.JobInstance);
+                if (attribute != null && !string.IsNullOrEmpty(attribute.Key))
+                {
+                    await Cronitor.Telemetries.CompleteAsync(attribute.Key, attribute.Message, attribute.Environment);
+                }
+            }
+        }
+
+        private CronitorIdentityAttribute GetAttribute(IJob job)
+        {
+            var type = job.GetType();
+            var attribute = (CronitorIdentityAttribute)Attribute.GetCustomAttribute(type, typeof(CronitorIdentityAttribute));
+
+            return attribute;
+        }
+    }
+}
